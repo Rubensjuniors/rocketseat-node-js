2f105b058dcf14211f986ca14fb2adb89ca9949a
Rubens junio <rubensjunio2110@gmail.com>
2024-11-19 21:37:46 -0300

    feat: create Rout e Delete

--- 01-fundamentos/src/server.js
+++ 01-fundamentos/src/server.js
@@ -1,41 +1,23 @@
 import http from 'node:http'
-import {json} from './middlewars/json.js'
+
+import { json } from './middlewars/json.js'
 import { routes } from './routes.js'
 
-// - Criar usuários
-// - Listagem usuários
-// - Edição de usuários
-// - Remoção de usuários
-
-// - HTTP
-//   - Método HTTP
-//   - URL
-
-// GET, POST, PUT, PATCH, DELETE
-
-// GET => Buscar um recurso do back-end
-// POST => Criar um recurso no back-end
-// PUT => Atualizar um recurso no back-end
-// PATCH => Atualizar uma informação específica de um recurso no back-end
-// DELETE => Deletar um recurso do back-end
-
-// GET /users => Buscando usuários no banc-end
-// POST /users => Criar um usuário no back-end
-
-// Stateful - Stateless
-// Cabeçalhos (Requisição/resposta) => Metadados
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-
   await json(req, res)
 
   const route = routes.find(route => {
-    return route.method === method && route.path === url
+    return route.method === method && route.path.test(url)
   })
 
-  if (route){
+  if (route) {
+    const routeParams = req.url.match(route.path)
+
+    req.params = { ...routeParams.groups }
+
     return route.handler(req, res)
   }
 
