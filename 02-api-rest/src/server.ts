fed459c6f819567aabe40f92276363600d69e8ff
rubens junio <rubens.marques@hotmart.com>
2024-11-21 10:11:26 -0300

    feat: added configs do db

--- 02-api-rest/src/server.ts
+++ 02-api-rest/src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
+import { knex } from './db/database'
 
 const server = fastify()
 
-server.get('/', () => {
-  return 'Hello World'
+server.get('/', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+  return tables
 })
 
 server
