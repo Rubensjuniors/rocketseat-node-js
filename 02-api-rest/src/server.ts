4b05ab9898a82b6e7605e9835f2b3b3c26f21b90
Rubens junio <rubensjunio2110@gmail.com>
2024-11-22 16:25:01 -0300

    feat: created amount

--- 02-api-rest/src/server.ts
+++ 02-api-rest/src/server.ts
@@ -1,16 +1,22 @@
 import fastify from 'fastify'
 import { knex } from './db/database'
+import { randomUUID } from 'node:crypto'
 
 const server = fastify()
 
 server.get('/', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transactions = await knex('transactions').insert({
+    id: randomUUID(),
+    title: 'Tranasaction Teste',
+    amount: 1000,
+  })
+
+  return transactions
 })
 
 server
   .listen({
-    port: 3333,
+    port: 4444,
   })
   .then(() => {
     console.log('HTTP Server Runner!!!')
