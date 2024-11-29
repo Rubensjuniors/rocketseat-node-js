1a9fbc7f7263b148a754e405447945517402e652
rubens junio <rubens.marques@hotmart.com>
2024-11-29 19:09:57 -0300

    feat: config database prisma

--- 03-api-solid/src/env/index.ts
+++ 03-api-solid/src/env/index.ts
@@ -10,6 +10,7 @@ if (process.env.NODE_ENV === 'test') {
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
