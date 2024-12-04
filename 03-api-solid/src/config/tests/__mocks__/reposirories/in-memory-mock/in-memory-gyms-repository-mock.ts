7c49a33ee8e226b4cd9636159c31fadcdea5db86
rubens junio <rubens.marques@hotmart.com>
2024-12-04 14:25:48 -0300

    feat created all use cases

--- 03-api-solid/src/config/tests/__mocks__/reposirories/in-memory-mock/in-memory-gyms-repository-mock.ts
+++ 03-api-solid/src/config/tests/__mocks__/reposirories/in-memory-mock/in-memory-gyms-repository-mock.ts
@@ -1,4 +1,8 @@
-import { GymsRepository } from '@/repositories/gyms-repository'
+import {
+  FindManyNearbyParams,
+  GymsRepository,
+} from '@/repositories/gyms-repository'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 import { Gym, Prisma } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
 
@@ -21,6 +25,25 @@ export class InMemoryGymsRepository implements GymsRepository {
     this.items.push(gym)
 
     return gym
+  }
+
+  async findManyNearby(params: FindManyNearbyParams) {
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        },
+      )
+      return distance < 10
+    })
+  }
+
+  async searchMany(query: string, page: number) {
+    return this.items
+      .filter((item) => item.title.includes(query))
+      .slice((page - 1) * 20, page * 20)
   }
 
   async findById(id: string) {
